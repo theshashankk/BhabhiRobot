83547a4cfccc4b29d987dbb22f92de502071b58d
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-03-21 13:16:26 -0700

    Update chatbot.py

--- MashaRoBot/modules/chatbot.py
+++ MashaRoBot/modules/chatbot.py
@@ -150,7 +150,6 @@ Chatbot utilizes the CoffeeHouse API and allows Masha to talk and provides a mor
  ❍ /rmchat*:* Disables Chatbot mode in the chat.
 
 Reports bugs at @{SUPPORT_CHAT}
-*Powered by CoffeeHouse* (https://coffeehouse.intellivoid.net/) from @Intellivoid
 """
 
 ADD_CHAT_HANDLER = CommandHandler("addchat", add_chat)
