6a592876ab3837e2106dd50f512b7e94cf72a4f9
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-03-22 08:17:11 -0700

    Update __tools.py

--- MashaRoBot/modules/__tools.py
+++ MashaRoBot/modules/__tools.py
@@ -1,10 +1,19 @@
 # © @Mr_Dark_Prince
 
-__mod_name__ = "▪TOOLS"
+__mod_name__ = "◎TOOLS"
 
 __help__ = """
- ❍ /encrypt :Encrypts The Given Text
- ❍ /decrypt :Decrypts Previously Ecrypted Text
+*Telegraph*
  ❍ /tm :Get Telegraph Link Of Replied Media
  ❍ /tt :Get Telegraph Link of Replied Text
+ 
+*Date-time-Weather*
+ ❍ /time <country code>*:* Gives information about a timezone.
+ ❍ /weather <city>*:* Get weather info in a particular place.
+ ❍ /wttr <city>*:* Advanced weather module, usage same as /weather
+ ❍ /wttr moon*:* Get the current status of moon
+ 
+*Other*
+ ❍ /encrypt*:* Encrypts The Given Text
+ ❍ /decrypt*:* Decrypts Previously Ecrypted Text
 """
