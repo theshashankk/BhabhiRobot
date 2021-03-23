632c517c3f4409d147872cfa565b349e79fc2dbc
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-03-23 03:32:44 -0700

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -375,7 +375,7 @@ def Masha_about_callback(update, context):
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
-                disable_web_page_preview=True,
+                disable_web_page_preview=false,
         )
 
 
@@ -402,7 +402,7 @@ def Source_about_callback(update, context):
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
-                disable_web_page_preview=True,
+                disable_web_page_preview=false,
         )
 
 @run_async
